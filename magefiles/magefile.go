622e7d25ee5e31acc8aca1ef496d1f35a9cbde36
Naofumi Okamoto <nokamoto.engr@gmail.com>
2024-02-23 07:22:10 +0000

    add

--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -7,6 +7,12 @@ import (
 )
 
 func Build() error {
+	if err := sh.Run("go", "install", "golang.org/x/tools/cmd/goimports@latest"); err != nil {
+		return err
+	}
+	if err := sh.Run("goimports", "-w", "."); err != nil {
+		return err
+	}
 	if err := sh.Run("go", "mod", "download"); err != nil {
 		return err
 	}
